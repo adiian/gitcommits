c9706d13f21a776297ab8f5f1475b5b46aea18ba
adiian <5307819+adiian@users.noreply.github.com>
2019-01-29 00:00:36 +0100

    added model entities

--- repos/models.py
+++ repos/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from projects.models import Project 
 
+# python manage.py makemigrations
 # Create your models here.
+class Repo(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.DO_NOTHING)
+    name = models.CharField(max_length=200)
+    values = models.TextField(blank=True)
+    def __str__(self): 
+        return self.name
