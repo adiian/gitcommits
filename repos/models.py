0bffcbf4fbe4e18bf861daad91662cde54f57ded
adiian <5307819+adiian@users.noreply.github.com>
2019-01-29 00:01:07 +0100

    Model comments

--- repos/models.py
+++ repos/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from projects.models import Project 
 
 # python manage.py makemigrations
+# python manage.py sqlmigrate listings 0001
+# python manage.py migrate
+
 # Create your models here.
 class Repo(models.Model):
     project = models.ForeignKey(Project, on_delete=models.DO_NOTHING)
