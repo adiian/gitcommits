3c7c9fca08ec3f0d08d2847f2acbb1cbd24557c2
adiian <5307819+adiian@users.noreply.github.com>
2019-01-29 12:20:49 +0100

    View fed with data from db

--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from projects.models import Project
+
 def index(request):
+    projects = Project.objects.all()
+
+    context = {
+        'projects': projects
+    }
+
+    return render(request, 'pages/index.html', context)
     return render(request, 'pages/index.html')
-    #return HttpResponse('<h1>Hello World</h1>')
 
 def about(request):
     return render(request, 'pages/about.html')
--- projects/views.py
+++ projects/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 
+from .models import Project
 # Create your views here.
-
+# fix VS error https://stackoverflow.com/questions/45135263/class-has-no-objects-member
 def index(request):
-    return render(request, 'projects/projects.html')
+    projects = Project.objects.all()
+
+    context = {
+        'projects': projects
+    }
+
+    return render(request, 'projects/projects.html', context)
 
 def project(request):
     return render(request, 'projects/project.html')
